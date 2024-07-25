af27a71d94216e438412445eb71fb758dfcccf4a
mirza-zain <mirzazain269@gmail.com>
2024-07-25 16:49:18 +0500

    Animation on loading screen added and Home page aniamtion updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import Navbar from './components/Navbar'
-import Home from './components/Home'
+import {React, useState, useEffect} from 'react';
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000)
+  }, []);
+
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      {
+        loading ? (
+          <div className='flex justify-center items-center w-full h-lvh'>
+          <ClimbingBoxLoader 
+          size={30}
+          color={'#1B3047'}
+          loading={loading}
+          />
+          </div>
+        ) : (
+          <>
+          <Navbar />
+          <Home />
+          </>
+        )   
+      }
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     return (
         <div className='container-fluid bg-transparent text-gray-950 flex flex-wrap lg:flex-nowrap '>
             <div className='flex justify-between items-center w-full md:w-auto'>
-                <a className='navbar-brand w-full' href='/'><img className='w-[25%] ' src={logo} alt="Logo" /></a>
+                <a className='navbar-brand w-full' href='/'><img className='w-[22%] ' src={logo} alt="Logo" /></a>
                 <div className='hamburger md:hidden text-2xl' onClick={toggleMobileMenu}>
                     <i className="ri-menu-line"></i>
                 </div>
@@ -22,13 +22,13 @@ const Navbar = () => {
             <div id='desktop' className='navbar hidden md:flex flex-nowrap'>
                 <ul className='navbar-nav flex-row gap-5'>
                     <li className='nav-item'>
-                        <a className='nav-link text-gray-950 text-xl font-semibold font-mono hover:text-orange-500 hover:ease-in ease-out duration-500' href='/home'>Home</a>
+                        <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 hover:ease-in ease-out duration-500 font-["Oswald"]' href='/home'>Home</a>
                     </li>
                     <li className='nav-item'>
-                        <a className='nav-link text-gray-950 text-xl font-semibold font-mono hover:text-orange-500 hover:ease-in ease-out duration-500' href='/character'>Characters</a>
+                        <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 hover:ease-in ease-out duration-500 font-["Oswald"]' href='/character'>Characters</a>
                     </li>
                     <li className='nav-item'>
-                        <a className='nav-link text-gray-950 text-xl font-semibold font-mono hover:text-orange-500 hover:ease-in ease-out duration-500' href='/blog'>Blog</a>
+                        <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 hover:ease-in ease-out duration-500 font-["Oswald"]' href='/blog'>Blog</a>
                     </li>
                 </ul>
             </div>
@@ -37,13 +37,13 @@ const Navbar = () => {
                 <div id='mobile' className='navbar md:hidden'>
                     <ul className='navbar-nav flex-col gap-5'>
                         <li className='nav-item'>
-                            <a className='nav-link text-gray-950 text-xl font-semibold font-mono hover:text-orange-500 ease-in-out duration-500' href='/home'>Home</a>
+                            <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 ease-in-out duration-500 font-["Oswald"]' href='/home'>Home</a>
                         </li>
                         <li className='nav-item'>
-                            <a className='nav-link text-gray-950 text-xl font-semibold font-mono hover:text-orange-500 ease-in-out duration-500' href='/character'>Characters</a>
+                            <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 ease-in-out duration-500 font-["Oswald"]' href='/character'>Characters</a>
                         </li>
                         <li className='nav-item'>
-                            <a className='nav-link text-gray-950 text-xl font-semibold font-mono hover:text-orange-500 ease-in-out duration-500' href='/blog' >Blog</a>
+                            <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 ease-in-out duration-500 font-["Oswald"]' href='/blog' >Blog</a>
                         </li>
                     </ul>
                 </div>
