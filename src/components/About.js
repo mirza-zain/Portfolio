e24b2168b45f3da64a382bb1bac17960c911d0e9
mirza-zain <mirzazain269@gmail.com>
2024-07-27 01:18:29 +0500

    About Me completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import Lottie from 'lottie-react';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import face from '../animations/face.json';
 import arrow from '../animations/arrow.json';
 import resume from '../files/MirzaZain.pdf'; 
 
 const About = () => {
+    const [heading] = useTypewriter({
+        words: ["bout Me"],
+        loop: true,
+        typeSpeed: 80,
+        deleteSpeed: 60
+    })
     return (
-        <div className='w-full h-screen flex flex-col md:flex-row'>
-            <div className='w-full md:w-1/2 flex h-full bg-black'>
-                <div className='bg-yellow-400 w-5 h-5 rounded-full'></div>
-                <div className='bg-yellow-400 w-20 h-20 rounded-full'></div>
+        <div className='w-full h-screen flex flex-col md:flex-row p-4'>
+            <div className='w-full md:w-1/2 bg-zinc-200'>
+                <div className='flex p-3'>
+                <div className='bg-yellow-400 w-8 md:w-10 h-8 md:h-10 rounded-full'></div>
+                <div className='bg-cyan-400 w-8 md:w-10 h-8 md:h-10 rounded-full'></div>
+                </div>
+                <h2 className='text-4xl p-4 font-["Playwrite_AR"] font-bold'>A{heading}<Cursor /></h2>
+                <p className='text-xl font-light font-sans w-[90%] p-4 text-start md:text-justify'>I'm Mirza Zain, a Full Stack Developer passionate about crafting exceptional web experiences. 
+                    Currently pursuing a Bachelor's degree in Software Engineering at Sindh Madressatul Islam University, 
+                    <br />I specialize in<span className='font-semibold '> "MERN (MongoDB, Express, React, Node)"</span>. 
+                    <br />With a keen eye for design and a strong foundation in development, 
+                    I'm committed to creating functional and visually appealing websites. 
+                    I'm always eager to learn and collaborate on exciting projects.
+                    Let's build something amazing together!
+                </p>
             </div>
-            <div className='w-full md:w-1/2 bg-gray-500 h-full flex flex-col '>
-                <Lottie animationData={face} className='w-full h-1/2 pt-4' />
+            <div className='w-full md:w-1/2 flex flex-col bg-emerald-200 h-[80%] md:h-full'>
+                <Lottie animationData={face} className='w-full h-[60%] md:h-[40%] pt-3 md:pt-2'/>
                 <a href={resume} className='px-8 py-3 font-bold text-lg rounded-md bg-yellow-400 mt-4 text-center' download>Resume</a>
-                <Lottie animationData={arrow} className='w-full h-[40%]' />
+                <Lottie animationData={arrow} className='w-full h-[25%] -rotate-45 pt-0' />
             </div>
         </div>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import logo from '../images/logo.png'
+import { useTypewriter } from 'react-simple-typewriter';
+// import logo from '../images/logo.png';
 
 const Navbar = () => {
     // State to manage mobile menu visibility
@@ -10,28 +11,34 @@ const Navbar = () => {
         setIsMobileMenuVisible(!isMobileMenuVisible);
     };
 
+    const [logo] = useTypewriter({
+        words: ["Mirza Zain"],
+        loop: true,
+        deleteSpeed: 80,
+    });
+
     return (
-        <div className='container-fluid bg-transparent text-gray-950 flex flex-wrap lg:flex-nowrap '>
+        <div className='container-fluid bg-transparent text-gray-950 flex flex-wrap lg:flex-nowrap justify-between pb-2'>
             <div className='flex justify-between items-center w-full md:w-auto'>
-                <a className='navbar-brand w-full' href='/'><img className='w-[22%] ' src={logo} alt="Logo" /></a>
+                <a className='navbar-brand w-full' href='/'><h2 className='font-["Playwrite_AR"] font-bold text-2xl text-emerald-600 px-3'>{logo}</h2></a>
                 <div className='hamburger md:hidden text-2xl' onClick={toggleMobileMenu}>
                     <i className="ri-menu-line"></i>
                 </div>
             </div>
             {/* Desktop Menu */}
             <div id='desktop' className='navbar hidden md:flex flex-nowrap'>
-                <ul className='navbar-nav flex-row gap-5'>
+                <ul className='navbar-nav flex-row gap-5 px-4'>
                     <li className='nav-item'>
-                        <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 ease-in-out duration-500 font-["Oswald"]' href='/home'>Home</a>
+                        <a className='nav-link text-gray-800 text-xl tracking-wider font-medium hover:text-emerald-500 ease-in-out duration-500 font-["Oswald"]' href='/home'>Home</a>
                     </li>
                     <li className='nav-item'>
-                        <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 ease-in-out duration-500 font-["Oswald"]' href='/about'>About Me</a>
+                        <a className='nav-link text-gray-800 text-xl tracking-wider font-medium hover:text-emerald-500 ease-in-out duration-500 font-["Oswald"]' href='/about'>About Me</a>
                     </li>
                     <li className='nav-item'>
-                        <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 ease-in-out duration-500 font-["Oswald"]' href='/project'>Projects</a>
+                        <a className='nav-link text-gray-800 text-xl tracking-wider font-medium hover:text-emerald-500 ease-in-out duration-500 font-["Oswald"]' href='/project'>Projects</a>
                     </li>
                     <li className='nav-item'>
-                        <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 ease-in-out duration-500 font-["Oswald"]' href='/contact'>Contact Me</a>
+                        <a className='nav-link text-gray-800 text-xl tracking-wider font-medium hover:text-emerald-500 ease-in-out duration-500 font-["Oswald"]' href='/contact'>Contact Me</a>
                     </li>
                 </ul>
             </div>
@@ -40,16 +47,16 @@ const Navbar = () => {
                 <div id='mobile' className='navbar md:hidden'>
                     <ul className='navbar-nav flex-col gap-5'>
                         <li className='nav-item'>
-                            <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 ease-in-out duration-500 font-["Oswald"]' href='/home'>Home</a>
+                            <a className='nav-link text-gray-800 text-xl tracking-wider font-medium hover:text-emerald-500 ease-in-out duration-500 font-["Oswald"]' href='/home'>Home</a>
                         </li>
                         <li className='nav-item'>
-                            <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 ease-in-out duration-500 font-["Oswald"]' href='/about'>About Me</a>
+                            <a className='nav-link text-gray-800 text-xl tracking-wider font-medium hover:text-emerald-500 ease-in-out duration-500 font-["Oswald"]' href='/about'>About Me</a>
                         </li>
                         <li className='nav-item'>
-                            <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 ease-in-out duration-500 font-["Oswald"]' href='/project' >Projects</a>
+                            <a className='nav-link text-gray-800 text-xl tracking-wider font-medium hover:text-emerald-500 ease-in-out duration-500 font-["Oswald"]' href='/project' >Projects</a>
                         </li>
                         <li className='nav-item'>
-                            <a className='nav-link text-gray-500 text-xl tracking-wider font-semibold hover:text-orange-500 ease-in-out duration-500 font-["Oswald"]' href='/contact' >Contact Me</a>
+                            <a className='nav-link text-gray-800 text-xl tracking-wider font-medium hover:text-emerald-500 ease-in-out duration-500 font-["Oswald"]' href='/contact' >Contact Me</a>
                         </li>
                     </ul>
                 </div>
