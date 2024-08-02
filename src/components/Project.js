a8cb85be1cc19b8169d8b7ae83e27e1a73078ae4
mirza-zain <mirzazain269@gmail.com>
2024-08-02 23:36:54 +0500

    Little changes

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,35 +6,35 @@ import pro4 from '../images/pro4.png';
 
 const Project = () => {
   return (
-    <div className='w-full h-full md:h-screen p-2 md:p-5'>
+    <div className='w-full h-full p-2 md:p-5'>
       <h2 className='text-[#E64833] text-3xl md:text-4xl font-bold font-["Playwrite_AR"] text-center md:text-start bg-[#FBE9D0] py-6 rounded-md'>My Project's</h2>
-      <div className='flex flex-wrap justify-center p-0 md:p-4'>
-        <div className='w-full md:w-[500px] relative p-2 bg-[#90AEAD]'>
-          <img className='block w-full' src={pro1} alt='project' />
-          <div className='absolute top-[60%] left-0 w-full h-full flex flex-col items-center justify-center text-black transition-opacity duration-300 opacity-0 hover:opacity-100'>
-            <h4 className='text-2xl font-semibold'>My Hero Academia</h4>
-            <p className='text-sm font-sans font-medium'>Created using React and Animation Library Lottie</p>
+      <div className='flex flex-col md:flex-row flex-wrap'>
+        <div className='w-full h-full md:w-1/2 p-2 bg-[#90AEAD]'>
+          <img className='w-full' src={pro1} alt='project' />
+          <div className='w-full h-full md:w-1/2'>
+            <h4 className='text-[#FBE9D0] text-2xl font-semibold'>My Hero Academia</h4>
+            <p className='text-black text-sm font-sans font-medium'>Created using React and Animation Library Lottie</p>
           </div>
         </div>
-        <div className='w-full sm:w-[500px] relative p-2 bg-[#90AEAD]'>
-          <img className='block w-full' src={pro2} alt='project' />
-          <div className='absolute top-[60%] left-0 w-full h-full flex flex-col items-center justify-center text-black transition-opacity duration-300 opacity-0 hover:opacity-100'>
-            <div className='text-2xl font-semibold'>Minions Page</div>
-            <p className='text-sm font-sans font-medium'>Created using HTML5, CSS, JavaScript</p>
+        <div className='w-full h-full md:w-1/2 p-2 bg-[#90AEAD]'>
+          <img className='w-full' src={pro2} alt='project' />
+          <div className='w-full h-full md:w-1/2'>
+            <h4 className='text-[#FBE9D0] text-2xl font-semibold'>Minions Web Page</h4>
+            <p className='text-black text-sm font-sans font-medium'>Created using HTML5, CSS and JavaScript</p>
           </div>
         </div>
-        <div className='w-full sm:w-[500px] relative p-2 bg-[#90AEAD]'>
-          <img className='block w-full' src={pro3} alt='project' />
-          <div className='absolute top-[60%] left-0 w-full h-full flex flex-col items-center justify-center text-black transition-opacity duration-300 opacity-0 hover:opacity-100'>
-            <div className='text-2xl font-semibold'>Fibonacci Web</div>
-            <p className='text-sm font-sans font-medium'>Created using HTML5 and CSS</p>
+        <div className='w-full h-full md:w-1/2 p-2 bg-[#90AEAD]'>
+          <img className='w-full' src={pro3} alt='project' />
+          <div className='w-full h-full md:w-1/2'>
+            <h4 className='text-[#FBE9D0] text-2xl font-semibold'>My Hero Academia</h4>
+            <p className='text-black text-sm font-sans font-medium'>Created using React and Animation Library Lottie</p>
           </div>
         </div>
-        <div className='w-full sm:w-[500px] relative p-2 bg-[#90AEAD]'>
-          <img className='block w-full' src={pro4} alt='project' />
-          <div className='absolute top-[60%] left-0 w-full h-full flex flex-col items-center justify-center text-black transition-opacity duration-300 opacity-0 hover:opacity-100'>
-            <div className='text-2xl font-semibold'>Mondrina Art</div>
-            <p className='text-sm font-sans font-medium'>Created using HTML5 and CSS</p>
+        <div className='w-full h-full md:w-1/2 p-2 bg-[#90AEAD]'>
+          <img className='w-full' src={pro4} alt='project' />
+          <div className='w-full h-full md:w-1/2'>
+            <h4 className='text-[#FBE9D0] text-2xl font-semibold'>My Hero Academia</h4>
+            <p className='text-black text-sm font-sans font-medium'>Created using React and Animation Library Lottie</p>
           </div>
         </div>
       </div>
